cdc2345da53d8665010ad519276e601f78d9ba55
AbirKundu <115333620+AbirKundu@users.noreply.github.com>
2022-12-17 00:36:51 +0600

    Update separeted float and int.py.py

--- python problems/separeted float and int.py.py
+++ python problems/separeted float and int.py.py
@@ -2,4 +2,17 @@ n=float(input())
 if n==int(n):
     print("int",int(n))
 else:
-    print("float",int(n),n-int(n))  
+    print("float",int(n),n-int(n)) 
+
+               ### factorial problem:::::::
+import math
+t=int(input())
+for i in range(t):
+    n=map(int,input().split())
+    for j in n:
+        print(math.factorial(j))
+        
+        
+        ### GCD problem::::::::
+        
+    
