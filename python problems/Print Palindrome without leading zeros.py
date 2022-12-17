4d02243163db95c280bf104da512b88e55dde85f
AbirKundu <115333620+AbirKundu@users.noreply.github.com>
2022-12-17 10:54:36 +0600

    Update Print Palindrome without leading zeros.py

--- python problems/Print Palindrome without leading zeros.py
+++ python problems/Print Palindrome without leading zeros.py
@@ -1,1 +1,10 @@
 
+N=input()   #we can take as a string
+p=N[::-1]   # reverse it using[::-1]
+
+print(int(p)) #as we working with integers so using int() function will delete leading zeros 2500 => 52
+if N==p:
+    print("YES")
+else:
+    print("NO")
+
