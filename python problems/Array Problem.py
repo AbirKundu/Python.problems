852de911be75e6f015e06e8d068143f6578290a9
AbirKundu <115333620+AbirKundu@users.noreply.github.com>
2022-12-19 20:29:26 +0600

    Update Array Problem.py

--- python problems/Array Problem.py
+++ python problems/Array Problem.py
@@ -1,1 +1,12 @@
+#### summation of index's in an Array.-----------------
 
+n=int(input())                             # lenthe of the array  = 4
+arr=list(map(int,input().split()))         # taking index's of an array as input = 7 3 2 1
+sum=0
+
+
+for i in range(len(arr)):                  # my loop will work between the lenth of the array (index[0] - len(array)) = 4
+    sum=sum+arr[i]                         # every time index will add up with the next index
+    arr[i]+=1
+print(abs(sum))
+    
